1ff73c9199e9f1ca66ce5431a1074c4722fbef00
Batuhan Sencer <batuhan.sencer@ttu.edu>
2026-04-13 11:59:58 -0500

    Update

--- eval/ept/ept_relation.sh
+++ eval/ept/ept_relation.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=ept_relation_students
 #SBATCH --partition=toreador
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=26
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=120G
 #SBATCH --time=48:00:00
 #SBATCH --output=eval/ept/benchmark/logs/%x_%j.out
